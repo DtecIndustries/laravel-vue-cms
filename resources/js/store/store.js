ecb25c3725928e76f70696511227e733a8eacb19
Joachim Defour <joachim.defour@icloud.com>
2020-05-26 21:47:40 +0200

    add vuex

---
+++ resources/js/store/store.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+const store = {
+    state: {},
+    modules: {},
+    mutations: {},
+    actions: {},
+    getters: {}
+};
+
+export default new Vuex.Store(store);
